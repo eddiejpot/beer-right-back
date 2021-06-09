d75e108f14df4b45ed3790b32214b8520a43d6bc
eddiejpot <jamesedwardporter@gmail.com>
2021-06-09 08:40:12 +0800

    added multer settings for linking it to heroku and s3

--- routes/authRouter.mjs
+++ routes/authRouter.mjs
@@ -3,8 +3,11 @@
 /* ============================================================================ */
 import express from 'express';
 
-// multer + set up
+// multer
 import multer from 'multer'; // npm install multer
+// multer + aws
+import aws from 'aws-sdk';
+import multerS3 from 'multer-s3';
 
 /* ============================================================================ */
 /* ======================================================= IMPORT CONTROLLERS = */
@@ -25,8 +28,30 @@ import {
   returnCookiesIfLoggedInMiddleware,
 } from '../utils/authFunctions.mjs';
 
+// configure libraries
+const s3 = new aws.S3({
+  accessKeyId: process.env.ACCESSKEYID,
+  secretAccessKey: process.env.SECRETACCESSKEY,
+});
+
+// congifure multer upload (muilter with aws s3)
+const multerUpload = multer({
+  storage: multerS3({
+    s3,
+    bucket: 'beerrightback',
+    acl: 'public-read',
+    metadata: (request, file, callback) => {
+      callback(null, { fieldName: file.fieldname });
+    },
+    key: (request, file, callback) => {
+      callback(null, Date.now().toString());
+    },
+  }),
+});
+
+// congifure multer upload (only on local)
 // set the name of the upload directory a.k.a image uploads go here
-const multerUpload = multer({ dest: 'uploads/' });
+// const multerUpload = multer({ dest: 'uploads/' });
 
 /* ============================================================ SET UP ROUTERS = */
 const router = express.Router();
